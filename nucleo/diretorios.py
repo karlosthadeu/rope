33a2f93a471b7d43196d62de46ef024679987db5
Hemenson David <hemensond@gmail.com>
2019-10-22 11:55:34 -0300

    adicao do arquivo de diretorios visando a organizacao das urls

---
+++ nucleo/diretorios.py
@@ -0,0 +1,24 @@
+'''
+
+Arquivo com constantes para organização das URL's
+
+'''
+
+#Página principal
+PRINCIPAL = r'principal'
+
+#Página em construção
+EM_CONSTRUCAO = r'em_construcao'
+
+#Chamados
+CHAMADOS = r'chamados'
+
+#Matérias
+MATERIAS = r'materias'
+
+#Planos de estudo
+PLANOS_DE_ESTUDO = r'planos_de_estudo'
+
+#Publicações
+PUBLICACOES = r'publicacoes'
+
