3a2a68b4c03977d80ba6a61c969b384cd0ed1c85
Hemenson David <hemensond@gmail.com>
2019-11-17 19:08:34 -0300

    adicionando as migracoes

--- rope/settings.py
+++ rope/settings.py
@@ -23,8 +23,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bootstrapform',
     'conta',
+    'bootstrapform',
     'nucleo',
 ]
 
